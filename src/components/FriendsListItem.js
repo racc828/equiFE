15c9d71f58ff7449b292e45ca8f7fbecb9b23973
github username <github email address>
2017-10-15 21:52:07 -0400

    bug fixed for following friends

--- src/components/FriendsListItem.js
+++ src/components/FriendsListItem.js
@@ -20,32 +20,10 @@ export default class FriendsList extends React.Component {
         <div className="list-container">
         <p>{this.props.friend.username} </p>
         {
-          this.props.followedFriends.length ===0 ?
-            <div><button onClick={this.followUser} className="float-right follow-user">Follow</button></div> : this.props.followedFriends.map((friend,i) => {
-              if (friend.id === this.props.friend.id) {
-                return <div><button key={i} onClick={this.unFollow} className="float-right unfollow-user">UnFollow</button></div>
-              } else {
-                return <div><button key={i} onClick={this.followUser} className="float-right follow-user">Follow</button></div> }
-            })
+          this.props.followedFriends.length > 0 && this.props.followedFriends.find(friend => friend.id === this.props.friend.id) ? <div><button onClick={this.unFollow} className="float-right unfollow-user">UnFollow</button></div> : <div><button onClick={this.followUser} className="float-right follow-user">Follow</button></div>
         }
         </div>
       </div>
     )
   }
-
-
 }
-
-
-//  {if(this.props.followedFriends.length === 0) {
-//   return <button onClick={this.followUser}>Follow</button>
-// } else {
-//   this.props.followedFriends.map((friend,i ) => {
-//     if (friend.id == this.props.friend.id) {
-//       return <button onClick={this.unFollow}>UnFollow</button>
-//     } else {
-//       return <button return <button onClick={this.followUser}>Follow</button>
-//     }
-//   })
-// }
-// }
