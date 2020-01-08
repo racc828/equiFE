8d81d2b268903e6d6bdf07fcc835e7b96e7eddb8
racc828 <racquel@dgtwo.com>
2020-01-07 19:21:36 -0500

    changed paths

--- src/adapters/SessionsAdapter.js
+++ src/adapters/SessionsAdapter.js
@@ -1,4 +1,4 @@
-const path = 'http://localhost:3000/api/v1/sessions'
+const path = 'https://equidestined-be.herokuapp.com/api/v1/sessions'
 export default class SessionsAdapter {
 
   static getUser(user){
